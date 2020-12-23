8067804b677983b7a35380cb44b6d4fde134fd72
Dmytro Berezhnyi <d.berezhnyi@vdata.com.ua>
2020-12-23 19:50:14 +0200

    log view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
@@ -16,14 +20,28 @@ import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var tvInfo: TextView
+
     private lateinit var executor: Executor
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
+    @SuppressLint("SetTextI18n")
+    fun showLogText(message: String) {
+        tvInfo.text = tvInfo.text.toString() + "\n" + message
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInfo = findViewById(R.id.tvInfo)
 
+        if ((getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager).isDeviceSecure) {
+            showLogText("Device is secure")
+        } else {
+            showLogText("Device is not secure")
+        }
         isAvailableAuth()
     }
 
@@ -33,27 +51,27 @@ class MainActivity : AppCompatActivity() {
             object : BiometricPrompt.AuthenticationCallback() {
                 override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
                     super.onAuthenticationError(errorCode, errString)
-                    Toast.makeText(applicationContext, "Authentication error: $errString", Toast.LENGTH_SHORT).show()
+                    showLogText("Authentication error: $errString")
                 }
 
                 override fun onAuthenticationSucceeded(
                     result: BiometricPrompt.AuthenticationResult
                 ) {
                     super.onAuthenticationSucceeded(result)
-                    Toast.makeText(applicationContext, "Authentication succeeded!", Toast.LENGTH_SHORT).show()
+                    showLogText("Authentication succeeded!")
                 }
 
                 override fun onAuthenticationFailed() {
                     super.onAuthenticationFailed()
-                    Toast.makeText(applicationContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    showLogText("Authentication failed")
                 }
             })
 
         promptInfo = BiometricPrompt.PromptInfo.Builder()
             .setTitle("Biometric login for my app")
             .setSubtitle("Log in using your biometric credential")
-            .setNegativeButtonText("NO") // Only Biometrics Auth
-            //.setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL) //Biometrics Auth or for example Pattern
+            //.setNegativeButtonText("NO") // Only Biometrics Auth
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL) //Biometrics Auth or for example Pattern
             .build()
 
         findViewById<Button>(R.id.btnAuth).setOnClickListener {
@@ -66,17 +84,18 @@ class MainActivity : AppCompatActivity() {
         when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
 
             BiometricManager.BIOMETRIC_SUCCESS -> {
-                Log.d("MY_APP_TAG", "App can authenticate using biometrics.")
+                showLogText("App can authenticate using biometrics.")
                 startBiometricAuth()
             }
 
             BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
-                Log.e("MY_APP_TAG", "No biometric features available on this device.")
+                showLogText("No biometric features available on this device.")
 
             BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
-                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.")
+                showLogText("Biometric features are currently unavailable.")
 
             BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                showLogText("BIOMETRIC_ERROR_NONE_ENROLLED")
                 // Prompts the user to create credentials that your app accepts.
                 val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
                     putExtra(
@@ -85,6 +104,15 @@ class MainActivity : AppCompatActivity() {
                     )
                 }
                 startActivityForResult(enrollIntent, 101)
+            }
+            BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> {
+                showLogText("BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED")
+            }
+            BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> {
+                showLogText("BIOMETRIC_ERROR_UNSUPPORTED")
+            }
+            BiometricManager.BIOMETRIC_STATUS_UNKNOWN -> {
+                showLogText("BIOMETRIC_STATUS_UNKNOWN")
             }
         }
     }
